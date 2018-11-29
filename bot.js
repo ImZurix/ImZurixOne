01e5f5ae7a0061d6daeba8ad50357fd482c293b4
ImZurix <36185818+ImZurix@users.noreply.github.com>
2018-11-29 14:21:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(``,"http://twitch.tv/")
+client.user.setGame(``,"http://twitch.tv/idk")
 client.user.setStatus("dnd")
 });
 
